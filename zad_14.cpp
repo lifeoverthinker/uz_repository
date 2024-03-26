f149085238329114897b8f86c724bdad885de990
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2024-03-26 19:01:59 +0100

    fix

--- zad_14.cpp
+++ zad_14.cpp
@@ -1,7 +1,15 @@
-// sprawdzenie czy można zbudować trójkąt
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+    int a = 3;
+    int b = 4;
+    int c = 5;
+
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        cout << "Mozna zbudowac trojkat" << endl;
+    } else {
+        cout << "Nie mozna zbudowac trojkata" << endl;
+    }
     return 0;
 }
