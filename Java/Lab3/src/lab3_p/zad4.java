dbab5ad84192e710c5eafb120ee537f4168c712b
lifeoverthinker <96396847+lifeoverthinker@users.noreply.github.com>
2024-10-30 10:01:56 +0100

    .

--- Java/Lab3/src/lab3_p/zad4.java
+++ Java/Lab3/src/lab3_p/zad4.java
@@ -1,4 +1,25 @@
 package lab3_p;
 
+import java.util.Scanner;
+
 public class zad4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Wciśnij '5' aby zakończyć");
+
+        int liczba;
+        int wynik = 1;
+
+        while (true) {
+            System.out.print("Podaj liczbę: ");
+            liczba = sc.nextInt();
+
+            if (liczba == 5) {
+                break;
+            }
+            wynik *= liczba;
+        }
+        System.out.println("Iloczyn liczb: " + wynik);
+    }
 }
--- Java/Lab3/src/lab3_p/zad5.java
+++ Java/Lab3/src/lab3_p/zad5.java
@@ -1,4 +1,25 @@
 package lab3_p;
 
+import java.util.Scanner;
+
 public class zad5 {
+    public static void main(String[] args) {
+        System.out.println("Podaj liczbe nieujemna: ");
+        Scanner sc = new Scanner(System.in);
+
+        int limit = sc.nextInt();
+
+        if(limit < 0){
+            System.out.println("Liczba nie jest nieujemna");
+        }else {
+            int potega = 1;
+
+            System.out.println("Potęgi liczby 3 mniejsze niż " + limit + ":");
+
+            while (potega < limit) {
+                System.out.println(potega);
+                potega *= 3;
+            }
+        }
+    }
 }
